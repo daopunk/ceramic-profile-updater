2451557fdb22b75deaf8485e1d093ce0a30ee94f
Hunter King <hking6776@gmail.com>
2022-11-10 17:00:19 +0100

    init

---
+++ pages/utils/dataclient.js
@@ -0,0 +1,5 @@
+import { CeramicClient } from '@ceramicnetwork/http-client';
+
+const ceramic = new CeramicClient('https://ceramic-clay.3boxlabs.com');
+
+export default ceramic;
---
+++ pages/utils/datamodel.js
@@ -0,0 +1,13 @@
+const aliases = {
+  schemas: {
+    basicProfile:
+      'ceramic://k3y52l7qbv1frxt706gqfzmq6cbqdkptzk8uudaryhlkf6ly9vx21hqu4r6k1jqio',
+  },
+  definitions: {
+    BasicProfile:
+      'kjzl6cwe1jw145cjbeko9kil8g9bxszjhyde21ob8epxuxkaon1izyqsu8wgcic',
+  },
+  titles: {},
+};
+
+export default aliases;
